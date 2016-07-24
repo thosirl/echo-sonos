60a21f4fb0db708772027514b38729797f88c878
thosirl <Tom.horan@gmail.com>
2016-07-24 16:59:35 +0100

    Added key & cert fields, for client-side certs

--- lambda/src/options.example.js
+++ lambda/src/options.example.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 // If you setup basic auth in node-sonos-http-api's settings.json, change the username
 // and password here.  Otherwise, just leave this alone and it will work without auth.
 var auth = new Buffer("YOUR_USERNAME" + ":" + "YOUR_PASSWORD").toString("base64");
@@ -10,6 +12,8 @@ var options = {
       'Authorization': 'Basic ' + auth,
       'Content-Type': 'application/json'
   },
+  key: fs.readFileSync('client.key'),
+  cert: fs.readFileSync('client.crt'),
   useHttps: false, // Change to true if you setup node-sonos-http-api with HTTPS
   rejectUnauthorized: true, // Change to false if you self-signed your certificate
 };
